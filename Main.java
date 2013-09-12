c384219365a8f4794f8d645677e32f6c56d89f31
PimpDuck <pimpduckpvp@hotmail.com>
2013-09-12 00:37:47 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,24 +1,73 @@
 package me.PimpDuck.DCRandomTP;
 
+import java.util.HashMap;
 import java.util.logging.Logger;
 
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class Main extends JavaPlugin{
 
-	  private CommandClass CommandClass = new CommandClass(this);
+public class Main extends JavaPlugin
+{
+  public YamlHelper yamlHandler;
+  public int minX;
+  public int maxX;
+  public int minZ;
+  public int maxZ;
+  public String world;
+  public static Main plugin;
+  public String tp_message;
 
-	Logger log = Logger.getLogger("Minecraft");
 
-	@Override
-	public void onEnable(){
-		log.info("[DCRandomTP] has been enabled!");
-        log.info("Copyright 2013 PimpDuck All rights reserved.");
-		saveDefaultConfig();
-        getCommand("randomtp").setExecutor(this.CommandClass);
-	}
+  Logger log = Logger.getLogger("Minecraft");
+  
+  public static FileManager configurationFile;
+  public static HashMap<String, Long> delayedPlayers;
+  private static int delay;
+  
+  public void onEnable()
+  {
+	saveDefaultConfig();
+    configurationFile = new FileManager(this, "config.yml");
+    delayedPlayers = new HashMap<String, Long>();
+    delay = getConfig().getInt("DCRandomTP.Options.Cooldown");
+    plugin = this;
+    this.yamlHandler = new YamlHelper(this);
+    this.minX = this.yamlHandler.config.getInt("DCRandomTP.Min-x", 1);
+    this.maxX = this.yamlHandler.config.getInt("DCRandomTP.Max-x", 100);
+    this.minZ = this.yamlHandler.config.getInt("DCRandomTP.Min-z", 1);
+    this.maxZ = this.yamlHandler.config.getInt("DCRandomTP.Max-z", 100);
+    this.world = this.yamlHandler.config.getString("DCRandomTP.World", "world");
+    this.tp_message = this.yamlHandler.config.getString("DCRandomTP.Message", "&aPlease wait while the plugin finds you a safe location.");
+    log.info("[DCRandomTP] has been enabled!");
+    getCommand("random").setExecutor(new CommandClass());
+  }
 	@Override
 	public void onDisable(){
-		log.info("[DCRandomTP] has been disabled!");	
+		log.info("[DCRandomTP] has been disabled!");
 	}
+	
+	  public static void addDelayedPlayer(Player player) {
+		    delayedPlayers.put(player.getName(), System.currentTimeMillis());
+		  }
+
+		  public static void removeDelayedPlayer(Player player) {
+		    delayedPlayers.remove(player.getName());
+		  }
+
+		  public static boolean playerDelayed(Player player) {
+		    return delayedPlayers.containsKey(player.getName());
+		  }
+
+		  public static Long getPlayerDelay(Player player) {
+		    return (Long)delayedPlayers.get(player.getName());
+		  }
+
+		  public static int getDelay(int multiplier) {
+		    return delay * multiplier;
+		  }
+
+		  public static int getRemainingTime(Player player) {
+		    return (int)(getDelay(1) - (System.currentTimeMillis() - getPlayerDelay(player).longValue()) / 1000L / 60 / 60);
+		  }
 }
